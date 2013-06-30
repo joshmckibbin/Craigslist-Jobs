e6314c76baacbb70a4345b990021c44e890b2618
joshmckibbin <joshmckibbin@gmail.com>
2013-06-30 13:43:12 -0400

    VERSION 1.5 UPDATE: Fixed 'No. of Entries' selection bug Completely new, sleek design Cleaned up

--- Craigslist-Jobs-Chrome/public_html/popup.js
+++ Craigslist-Jobs-Chrome/public_html/popup.js
@@ -1,42 +1,25 @@
-function linkify(inputText) {
-    var replaceText, replacePattern1, replacePattern2, replacePattern3;
-
-    //URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
-
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
-
-    //Change email addresses to mailto:: links.
-    replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1" target="_blank">$1</a>');
-
-    return replacedText;
-}
 function parseXml(xml){
-    $(xml).find("item").each(function(){
-        var title, description, link;
-        title = $(this).find("title").first().text();
-        description = $(this).find("description").text();
+    $(xml).find("item").slice(0,localStorage["entry_qty"]).each(function(){
+        var title = $(this).find("title").first().text(),
+        description = $(this).find("description").text(),
         link = $(this).find("link").text();
         
-        description = description.replace("[...]","...");
-        description = description + " (<a href=\""+link+"\" target=\"_blank\">View Full Post</a>)";
+        description = description.replace(" [...]","...");
+        description = description + "<a href=\""+link+"\" target=\"_blank\">View Full Listing</a>";
         
         $("#main-content").append("<div class=\"entry\"><h2><a href=\"" + link + "\" target=\"_blank\">" + title + "</a></h2><p>" + description + "</p></div>");
     });
     
+    /* Slide description on h2 click */
     $(".entry h2 a").click(function(e){
         $('.entry p').not($(this).parent().siblings("p")).slideUp();
-        $(this).parent().siblings("p").slideToggle();
+        $(this).css('color','rgb(13,68,110)').parent().siblings("p").slideToggle();
+     //   $(this).css('color','rgb(13,68,110)').parent().css('font-weight','normal').siblings("p").slideToggle();
         e.preventDefault();
-    });
+    });    
 }
 
 $(function(){
-    var qty = localStorage["entry_qty"];
     var type = localStorage["job_type"];
     var city = localStorage["city"];
     switch(city){
